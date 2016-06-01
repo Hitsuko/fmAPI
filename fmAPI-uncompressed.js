a3c2dfaaffbead6af5a4127cb9fdf4faa4ae55f5
Hitsuko <memey.genius@gmail.com>
2016-06-01 20:12:24 +0800

    Update fmAPI-uncompressed.js

--- fmAPI-uncompressed.js
+++ fmAPI-uncompressed.js
@@ -1,14 +1,21 @@
+// WIP!
 var $fm = {
  version:0.1, // the version of the code
- use: false, // if the value was TRUE, then you can use the code. if the value was FALSE, then you can't use the code. this would also prevent from using it when it's false
+ use: false, // if the value was TRUE, then you can use the code. if the value was FALSE, then you can't use the code. this would also prevent  from using it when it's false. this feature will be disabled on version 1.0.
  author:'Hitsu', // obviously. it was me who'd make the code
- cookie: { // cookie functions...
-  cook: function(name,value) { my_setcookie(name,value); }, // declare a new cookie
-  take: function(name) { return my_getcookie(name); }, // get a cookie
-  eat: function(name) { document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'; console.log('Yummy cookies!');} // delete a cookie
+ cookie: {
+  cook: function(name,value) { my_setcookie(name,value); },
+  take: function(name) { return my_getcookie(name); },
+  eat: function(name) { document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'; console.log('Yummy cookies!');}
  },
- // on construction.
+  forumData: {
+  username: return _userdata.username;
+  guest: function () { if(_userdata.session_logged_in = 0) { return true; }else{ return false; } }
+  member: function() { if(_userdata.session_logged_in = 1) { return true; }else{ return false; } }
+  memberAvatar: return _userdata.avatar;
+ }
 };
-if($fm.use === false) { var $fm=false; } // you can't use it when the 'use' property was false
-if($fm.version < 0.1) { var $fm=false; console.log('Please update to 0.1 first.'); } // this would ask you to update the script when you're at the lower version
-if($fm.author != 'Hitsu') { alert('This code was originally written by Hitsu. Do not reclaim it as ' + $fm.author + ' !'); } // this would give you an alert when you'd change the author's name
+// a few traps. lol
+if($fm.use === false) { var $fm=false; }
+if($fm.version < 0.1) { var $fm=false; console.log('Please update to 0.1 first.'); }
+if($fm.author != 'Hitsu') { alert('This code was originally written by Hitsu. Do not reclaim it as ' + $fm.author + ' !'); }
