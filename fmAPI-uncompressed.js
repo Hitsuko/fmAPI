a67283627d193310e3d53ce2d1c4978732f773ad
Hitsuko <memey.genius@gmail.com>
2016-06-16 10:22:15 +0800

    Update fmAPI-uncompressed.js

--- fmAPI-uncompressed.js
+++ fmAPI-uncompressed.js
@@ -1,6 +1,6 @@
 (function() {
  $fm = {
-  version:"1.4.4",
+  version:"1.4.6",
   author:"Hitsu",
   cookie: {
    cook: function(name,value) { my_setcookie(name,value); },
@@ -8,27 +8,23 @@
    eat:  function(name) { document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:01 GMT;"; console.log("Yummy cookies!"); }
   },
   forumData: {
-   username: _userdata.username,
-   member: function() { if(_userdata.session_logged_in) { return true; }else{ return false; } },
-   memberAvatar: _userdata.avatar,
-   isTopic: function() { if(window.location.pathname.indexOf("/t") > -1) { return true; }else{ return false; } },
+   memberData: [_userdata.avatar, _userdata.user_id, _userdata.user_posts, _userdata.user_nb_privmsg, _userdata.point_reputation, _userdata.user_lang],
+   member: _userdata.session_logged_in != -1 ? true : false,
+   isTopic: window.location.pathname.indexOf("/t") > -1 ? true : false,
    isTopicID: function(topicid) { if(window.location.pathname.indexOf("/t") && window.location.pathname.indexOf(topicid)) { return true; }else{ return false; } },
-   isPM: function() { if(window.location.pathname.indexOf("/privmsg")) { return true; }else{ return false; } },
-   memberID: _userdata.user_id,
-   isAdmin: function() { if(_userdata.user_level) { return true; }else{ return false; } },
-   memberPosts: _userdata.user_posts,
-   memberPM: _userdata.user_nb_privmsg,
-   memberRep: _userdata.point_reputation,
-   memberLang: _userdata.user_lang
+   isPM: function() { window.location.pathname.indexOf("/privmsg") > -1 ? true : false; },
+   isAdmin: _userdata.user_level !== 0 ? true : false,
+   isViewUser: document.title.indexOf("Viewing profile") != -1 ? true : false,
+   isViewUserID: function(userid) { if(window.location.pathname.indexOf("/u") !== -1 && window.location.pathname.indexOf(userid) !== -1) { return true; }else{ return false; }
   },
   utilities: {
    log: function(text) { console.log(text); },
    warning: function(text) { console.warn(text); },
-   boardInformation: {
-     boardURL: window.location.hostname
+   error: function(text) { console.error(text); },
+   boardInformation: [window.location.hostname]
    }
   }
  };
-  if($fm.version != "1.4.4") { $fm = false; console.log("Please update to 1.4.4 first."); }
+  if($fm.version != "1.4.6") { $fm = false; console.log("Please update to 1.4.6 first."); }
   if($fm.author != "Hitsu") { document.innerHTML = "The original author of fmAPI was Hitsu. Don't steal from her."; }
 }());
